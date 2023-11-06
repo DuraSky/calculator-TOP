6bace4cd15d35376dd4e4f80e63c6a173f7e3e43
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-11-06 12:01:18 +0100

    added working display and reworked = to call operate() instead. now need to fix behaviour where the calculator does not evaluate after inputting two numbers

--- script.js
+++ script.js
@@ -1,125 +1,25 @@
-// once you have finalResult, clear the inputArr, but keep finalResult
-
+//user can enter one full array with an operator
+//once an operator is pressed, check if leftNumber is null 
+// if not, run operate(), if yes, let user input a number
 
 let inputArr = [];
 let finalResult = 0;
+const display = document.querySelector("#calculatorDisplay");
+// let rightNumber = 0;
+// let leftNumber = 0;
+
 
 function clearArray() {
      inputArr.splice(0, inputArr.length);
-     inputArr.push(finalResult);
-    
+     inputArr.push(finalResult);   
 };
 
+function updateDisplay(){
+    let modifInputArr = inputArr.join('');
+    return modifInputArr;
+};
 
-const button0 = document.querySelector("#number0");
-button0.addEventListener("click", () =>{
-    inputArr.push(0);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const button1 = document.querySelector("#number1");
-button1.addEventListener("click", () =>{
-    inputArr.push(1);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const button2 = document.querySelector("#number2");
-button2.addEventListener("click", () =>{
-    inputArr.push(2);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const button3 = document.querySelector("#number3");
-button3.addEventListener("click", () =>{
-    inputArr.push(3);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const button4 = document.querySelector("#number4");
-button4.addEventListener("click", () =>{
-    inputArr.push(4);
-    console.log(inputArr);
-    return inputArr;
-});
-
-const button5 = document.querySelector("#number5");
-button5.addEventListener("click", () =>{
-    inputArr.push(5);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const button6 = document.querySelector("#number6");
-button6.addEventListener("click", () =>{
-    inputArr.push(6);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const button7 = document.querySelector("#number7");
-button7.addEventListener("click", () =>{
-    inputArr.push(7);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const button8 = document.querySelector("#number8");
-button8.addEventListener("click", () =>{
-    inputArr.push(8);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const button9 = document.querySelector("#number9");
-button9.addEventListener("click", () =>{
-    inputArr.push(9);
-    console.log(inputArr);
-    return inputArr;
-    
-});
-
-const plusOperator = document.querySelector("#plusOperator");
-plusOperator.addEventListener("click", () =>{
-    inputArr.push("+");
-    console.log(inputArr);
-    return inputArr;
-});
-
-const minusOperator = document.querySelector("#minusOperator");
-minusOperator.addEventListener("click", () =>{
-    inputArr.push("-");
-    console.log(inputArr);
-    return inputArr;
-});
-
-const timesOperator = document.querySelector("#timesOperator");
-timesOperator.addEventListener("click", () =>{
-    inputArr.push("*");
-    console.log(inputArr);
-    return inputArr;
-});
-
-const divisionOperator = document.querySelector("#divisionOperator");
-divisionOperator.addEventListener("click", () =>{
-    inputArr.push("/");
-    console.log(inputArr);
-    return inputArr;
-});
-
-const getResult = document.querySelector("#getResult");
-getResult.addEventListener("click", () =>{
+function operate(){
     const whereOperator = inputArr.findIndex(item => item === "+" || item === "-" || item === "*" || item === "/");
     console.log("Index of the operator is: " + whereOperator);
     const rightSide = inputArr.slice(0, whereOperator);
@@ -127,14 +27,9 @@ getResult.addEventListener("click", () =>{
     const leftSide = inputArr.slice(whereOperator + 1);
     console.log(leftSide);
 
-   let rightNumber = rightSide.reduce((current, next)=>{
-        return (current * 10) + next;
-   },0);
-   console.log("Right number: " + rightNumber);
-
-   const leftNumber = leftSide.reduce((current, next)=>{
-    return (current * 10) + next;
-    },0);
+    let rightNumber = parseFloat(rightSide.join('')); 
+    console.log("Right number: " + rightNumber);
+    let leftNumber = parseFloat(leftSide.join('')); 
     console.log("Left number: " + leftNumber);
 
     if(inputArr[whereOperator] === "+"){
@@ -146,34 +41,134 @@ getResult.addEventListener("click", () =>{
     } else if (inputArr[whereOperator] === "/"){
         finalResult = rightNumber / leftNumber;
     }
-    console.log("Final result is: " + finalResult);
-    clearArray();
+
+    return rightNumber+leftNumber;
+};
+
+
+const button0 = document.querySelector("#number0");
+button0.addEventListener("click", () =>{
+    inputArr.push(0);
+    console.log(inputArr);  
+    display.value = updateDisplay();
+    
 });
 
+const button1 = document.querySelector("#number1");
+button1.addEventListener("click", () =>{
+    inputArr.push(1);
+    console.log(inputArr);
+    display.value = updateDisplay();
+    
+});
 
+const button2 = document.querySelector("#number2");
+button2.addEventListener("click", () =>{
+    inputArr.push(2);
+    console.log(inputArr);
+    display.value = updateDisplay();
+    
+});
 
+const button3 = document.querySelector("#number3");
+button3.addEventListener("click", () =>{
+    inputArr.push(3);
+    console.log(inputArr);
+    display.value = updateDisplay();
+    
+});
 
+const button4 = document.querySelector("#number4");
+button4.addEventListener("click", () =>{
+    inputArr.push(4);
+    console.log(inputArr);
+    display.value = updateDisplay();
+});
 
+const button5 = document.querySelector("#number5");
+button5.addEventListener("click", () =>{
+    inputArr.push(5);
+    console.log(inputArr);
+    display.value = updateDisplay();
+    
+});
 
+const button6 = document.querySelector("#number6");
+button6.addEventListener("click", () =>{
+    inputArr.push(6);
+    console.log(inputArr);
+    display.value = updateDisplay();
+    
+});
 
+const button7 = document.querySelector("#number7");
+button7.addEventListener("click", () =>{
+    inputArr.push(7);
+    console.log(inputArr);
+    display.value = updateDisplay();
+    
+});
 
+const button8 = document.querySelector("#number8");
+button8.addEventListener("click", () =>{
+    inputArr.push(8);
+    console.log(inputArr);
+    display.value = updateDisplay();
+    
+});
 
+const button9 = document.querySelector("#number9");
+button9.addEventListener("click", () =>{
+    inputArr.push(9);
+    console.log(inputArr);
+    display.value = updateDisplay();
+    
+});
 
+const plusOperator = document.querySelector("#plusOperator");
+plusOperator.addEventListener("click", () =>{
+    inputArr.push("+");
+    console.log(inputArr);
+    display.value = updateDisplay();
 
+    if(leftNumber.length != 0){
+        operate();
+    }
+});
 
+const minusOperator = document.querySelector("#minusOperator");
+minusOperator.addEventListener("click", () =>{
+    inputArr.push("-");
+    console.log(inputArr);
+    display.value = updateDisplay();
+});
 
+const timesOperator = document.querySelector("#timesOperator");
+timesOperator.addEventListener("click", () =>{
+    inputArr.push("*");
+    console.log(inputArr);
+    display.value = updateDisplay();
+});
 
+const divisionOperator = document.querySelector("#divisionOperator");
+divisionOperator.addEventListener("click", () =>{
+    inputArr.push("/");
+    console.log(inputArr);
+    display.value = updateDisplay();
+});
 
+const dot = document.querySelector("#dot");
+dot.addEventListener("click", () =>{
+    inputArr.push(".");
+    console.log(inputArr);
+    display.value = updateDisplay();
+});
 
-
-
-
-
-
-// const arrayTest = document.querySelector("#testingArray");
-// arrayTest.addEventListener("click", () =>{
-//     console.log(inputArr);
-// });
-
-
+const getResult = document.querySelector("#getResult");
+getResult.addEventListener("click", () =>{
+    operate();
+    console.log("Final result is: " + finalResult);
+    clearArray();
+    display.value = updateDisplay();
+});
 
