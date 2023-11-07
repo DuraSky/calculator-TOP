4f114b26c267e558b7ea3c721adff08af7383f96
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-11-07 10:33:49 +0100

    all the required calculator logic now works, only thing left to do is styling

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
-//user can enter one full array with an operator
-//once an operator is pressed, check if leftNumber is null 
-// if not, run operate(), if yes, let user input a number
-
 let inputArr = [];
 let finalResult = 0;
 const display = document.querySelector("#calculatorDisplay");
-// let rightNumber = 0;
-// let leftNumber = 0;
-
 let leftSideDefined = false;
 
-
-function clearArray() {
-     inputArr.splice(0, inputArr.length - 1);
-     inputArr.unshift(finalResult);
-    //  leftSideDefined = false;  
-    
-     console.log(inputArr);
+function disableDot(){
+    dot.disabled = true;
 };
 
+function enableDot(){
+    dot.disabled = false;
+}
+
+function clearArray() {   
+     inputArr.splice(0, inputArr.length - 1); 
+     inputArr.unshift(finalResult);   
+     console.log(inputArr);
+};
 
 function updateDisplay(){
     let modifInputArr = inputArr.join('');
     return modifInputArr;
 };
 
-
-
-function operate(){
-    //  if (leftSideDefined) {
-    
+function operate(){   
     const whereOperator = inputArr.findIndex(item => item === "+" || item === "-" || item === "*" || item === "/");
     console.log("Index of the operator is: " + whereOperator);
     const rightSide = inputArr.slice(0, whereOperator);
@@ -39,9 +32,7 @@ function operate(){
     let rightNumber = parseFloat(rightSide.join('')); 
     console.log("Right number: " + rightNumber);
     let leftNumber = parseFloat(leftSide.join('')); 
-    console.log("Left number: " + leftNumber);
-
-    
+    console.log("Left number: " + leftNumber);   
 
     if(inputArr[whereOperator] === "+"){
         finalResult = rightNumber + leftNumber;
@@ -51,43 +42,43 @@ function operate(){
         finalResult = rightNumber * leftNumber;
     } else if (inputArr[whereOperator] === "/"){
         finalResult = rightNumber / leftNumber;
+    } else if (inputArr[whereOperator] === null){
+        rightNumber = finalResult;
     }
-    clearArray();
 
-// }   
+    if(finalResult == "Infinity"){
+        alert("Whatchu tryin to do, reset the calc brother");
+    }
+    
+    clearArray();    
 };
-
 
 const button0 = document.querySelector("#number0");
 button0.addEventListener("click", () =>{
     inputArr.push(0);
     console.log(inputArr);  
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay();  
 });
 
 const button1 = document.querySelector("#number1");
 button1.addEventListener("click", () =>{
     inputArr.push(1);
     console.log(inputArr);
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay(); 
 });
 
 const button2 = document.querySelector("#number2");
 button2.addEventListener("click", () =>{
     inputArr.push(2);
     console.log(inputArr);
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay();  
 });
 
 const button3 = document.querySelector("#number3");
 button3.addEventListener("click", () =>{
     inputArr.push(3);
     console.log(inputArr);
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay();   
 });
 
 const button4 = document.querySelector("#number4");
@@ -101,51 +92,46 @@ const button5 = document.querySelector("#number5");
 button5.addEventListener("click", () =>{
     inputArr.push(5);
     console.log(inputArr);
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay();   
 });
 
 const button6 = document.querySelector("#number6");
 button6.addEventListener("click", () =>{
     inputArr.push(6);
     console.log(inputArr);
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay();  
 });
 
 const button7 = document.querySelector("#number7");
 button7.addEventListener("click", () =>{
     inputArr.push(7);
     console.log(inputArr);
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay();   
 });
 
 const button8 = document.querySelector("#number8");
 button8.addEventListener("click", () =>{
     inputArr.push(8);
     console.log(inputArr);
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay();   
 });
 
 const button9 = document.querySelector("#number9");
 button9.addEventListener("click", () =>{
     inputArr.push(9);
     console.log(inputArr);
-    display.value = updateDisplay();
-    
+    display.value = updateDisplay();  
 });
 
 const plusOperator = document.querySelector("#plusOperator");
-plusOperator.addEventListener("click", () =>{
-    
+plusOperator.addEventListener("click", () =>{   
     if (leftSideDefined) {
         inputArr.push("+");
         console.log("if condition for plus: " +inputArr);
         operate();
     } else {
         inputArr.push("+");
+        enableDot();
         console.log("else condition for plus: " +inputArr);
         leftSideDefined = true; // Set the flag to true when entering an operator
     }
@@ -160,6 +146,7 @@ minusOperator.addEventListener("click", () =>{
         operate();
     } else {
         inputArr.push("-");
+        enableDot();
         console.log("else condition for plus: " +inputArr);
         leftSideDefined = true; // Set the flag to true when entering an operator
     }
@@ -174,6 +161,7 @@ timesOperator.addEventListener("click", () =>{
         operate();
     } else {
         inputArr.push("*");
+        enableDot();
         console.log("else condition for plus: " +inputArr);
         leftSideDefined = true; // Set the flag to true when entering an operator
     }
@@ -188,6 +176,7 @@ divisionOperator.addEventListener("click", () =>{
         operate();
     } else {
         inputArr.push("/");
+        enableDot();
         console.log("else condition for plus: " +inputArr);
         leftSideDefined = true; // Set the flag to true when entering an operator
     }
@@ -198,14 +187,28 @@ const dot = document.querySelector("#dot");
 dot.addEventListener("click", () =>{
     inputArr.push(".");
     console.log(inputArr);
+    disableDot();
     display.value = updateDisplay();
 });
 
 const getResult = document.querySelector("#getResult");
 getResult.addEventListener("click", () =>{
     operate();
+    inputArr.splice(1, inputArr.length - 1);
+    leftSideDefined = false;
     console.log("Final result is: " + finalResult);
-    display.value = finalResult;
-    
+    display.value = finalResult;   
 });
 
+const resetCalc = document.querySelector("#resetCalc");
+resetCalc.addEventListener("click", () =>{
+    inputArr.splice(0, inputArr.length);
+    display.value = updateDisplay();
+});
+
+const backspace = document.querySelector("#backspace");
+backspace.addEventListener("click", () =>{
+    inputArr.splice(inputArr.length -1, 1);
+    leftSideDefined = false;
+    display.value = updateDisplay();
+});
